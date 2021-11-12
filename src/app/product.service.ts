ddff05eeab7d178d2566af63035a980a0f54403f
radelapaz <ra.delapaz04@gmail.com>
2021-11-12 12:32:31 +0800

    Generated and modified product-lists

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map } from "rxjs/operators"; 
 
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
@@ -11,12 +12,18 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   getAlbum(id:number): Observable<Album> {
     return this._http.get(this._albumUrl)
     .pipe(map(response => <Album>response.json()));
 
 
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl)
+    .map(response => <Product[]>response.json());
   }
 
 }
